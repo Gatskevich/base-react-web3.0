7b8dc9ef87b0caf4d206442ec5c031c1bd6bdb3c
rodionhatskevich <rodionhatskevich@gmail.com>
2023-07-30 22:39:59 +0200

    create welcome sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Navbar } from './components';
+import { Navbar, Welcome } from './components';
 
 function App() {
   return (
     <div className="min-h-screen">
       <div className="gradient-bg-welcome">
         <Navbar />
+        <Welcome />
       </div>
     </div>
   );
