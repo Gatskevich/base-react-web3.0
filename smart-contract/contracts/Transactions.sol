6e144970d5e4e9d60bb331927400673a81e7b544
rodionhatskevich <rodionhatskevich@gmail.com>
2023-08-18 18:13:11 +0200

    set up hardhat

--- smart-contract/contracts/Transactions.sol
+++ smart-contract/contracts/Transactions.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
 contract Transactions {
     uint256 transactionCount;
 
--- smart-contract/hardhat.config.ts
+++ smart-contract/hardhat.config.ts
@@ -3,6 +3,12 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    goelri: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/CadRKnQwwqxYXfj_cMgo8aVsGzH5jBno',
+      accounts: ['709375970c2bbbc9c59e02400b38c44951ec6955422a1c4dfc77fe4480404f5d'],
+    },
+  },
 };
 
 export default config;
